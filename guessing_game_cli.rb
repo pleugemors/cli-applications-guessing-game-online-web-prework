b5accf45e23d11df244e8a4dafb637b9e8edbf97
Paul Leugemors <pleugemors@yahoo.com>
2019-10-30 06:23:34 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,6 @@ def run_guessing_game
   puts "Guess number between 1 and 6"
   answer = gets.chomp
   if r_num == answer.to_i
-    puts "hello"
     puts "You guessed the correct number!"
   elsif answer == "exit"
     puts "Goodbye!"
